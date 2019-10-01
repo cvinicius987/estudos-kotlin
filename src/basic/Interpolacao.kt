2ae1b34855b3c82296b5e096a23133ae060715de
Caio <caio.tecnologia@gmail.com>
2019-10-01 00:18:24 -0300

    Estudos de kotlin

---
+++ src/basic/Interpolacao.kt
@@ -0,0 +1,9 @@
+package basic
+
+fun name(name:String):String =  "A Linguagem é $name"
+		
+fun main(args: Array<String>){
+	println(name("Kotlin"))
+	println(name("Java"))
+	println(name("Go"))
+}
---
+++ src/basic/Var.kt
@@ -0,0 +1,9 @@
+package basic
+
+//Imutavel
+val str 		= "Nome...."
+val str2:String = "Nome 2"
+val valor 		= 4.25
+
+//Mutavel
+var teste = "Teste"
