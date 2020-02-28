3c08b54a27109df37d76889e733efeeb9bcad6c9
Caio <caio.tecnologia@gmail.com>
2020-02-27 23:45:52 -0300

    Exercicios função de alta ordem como parametro, com java e retorno.

--- src/cap8/funcao_alta_ordem/fun_invoke_from_java.kt
+++ src/cap8/funcao_alta_ordem/fun_invoke_from_java.kt
@@ -1,2 +1,3 @@
 package cap8.funcao_alta_ordem
 
+fun invokeFromJava(msg:String, convertToInt:(String) -> Int) = println(convertToInt(msg))
---
+++ src/cap8/funcao_alta_ordem/fun_param_default_null.kt
@@ -0,0 +1,14 @@
+
+fun executeConsumer(consumer:(String) -> Unit = { println(it)}){
+	
+	val list = listOf("java", "Kotlin", "PHP", "Golang")
+	
+	list.forEach(consumer)
+}
+
+fun main(args: Array<String>) {
+	
+	executeConsumer()
+	
+	executeConsumer({ println(it.toUpperCase())})
+}
